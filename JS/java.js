e7f4ea301801f082730abaeb0918712fd3c8d4d4
AntiViruS90 <131804906+AntiViruS90@users.noreply.github.com>
2023-09-04 13:20:32 +0300

    Add files via upload

---
+++ JS/java.js
@@ -0,0 +1,52 @@
+$(document).ready(function () {
+
+    $('.jpg12').fadeOut().fadeIn(1000);
+    $('.cook-text h2').hide().slideDown(3000);
+
+    $(window).scrollTop(function () {
+        $('.icons').hide().fadeIn(5000);
+        $('.icons img').hide().show(6000);
+        $('.jpg13').hide().fadeIn(4500);
+        $('.block3-text-circle-div').hide().fadeIn(5500);
+        $('.block3-orange-circle-div').hide().fadeIn(7000);
+        $('.small-white-circle-div').hide().fadeIn(7000);
+        $('.jpg15').hide().fadeIn(5000);
+        $('.jpg14').hide().fadeIn(6000)
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
